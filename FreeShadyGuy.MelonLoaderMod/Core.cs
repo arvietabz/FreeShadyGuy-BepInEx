d5bd02b32ad90e57d4228fab315400056f0cde88
Slimaeus <nguyenhongthai28042002@gmail.com>
2025-11-06 08:47:17 +0700

    Add project files.

---
+++ FreeShadyGuy.MelonLoaderMod/Core.cs
@@ -0,0 +1,24 @@
+﻿using HarmonyLib;
+using Il2CppAssets.Scripts.Inventory__Items__Pickups.GoldAndMoney;
+using MelonLoader;
+
+[assembly: MelonInfo(typeof(FreeShadyGuy.MelonLoaderMod.Core), "FreeShadyGuy", "1.0.0", "Slimaeus", null)]
+[assembly: MelonGame("Ved", "Megabonk")]
+
+namespace FreeShadyGuy.MelonLoaderMod
+{
+    public class Core : MelonMod
+    {
+        [HarmonyPatch(typeof(MoneyUtility))]
+        internal static class MoneyUtilityPatches
+        {
+            [HarmonyPatch("GetItemPriceShadyGuy")]
+            [HarmonyPrefix]
+            private static bool GetItemPriceShadyGuy_Prefix(ref int __result)
+            {
+                __result = 0;
+                return false;
+            }
+        }
+    }
+}
